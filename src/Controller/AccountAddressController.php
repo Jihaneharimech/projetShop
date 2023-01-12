1ddc09110ec03d4ef9582f003cf07e3f91beabf7
jihane <jihane.harimech@gmail.com>
2023-01-12 23:33:36 +0100

    Creat56n de fonction ajouter adresse

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use App\Entity\Adress;
 use App\Form\AddressType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AccountAddressController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/compte/adresses', name: 'app_account_address')]
     public function index(): Response
     {
@@ -18,11 +28,22 @@ class AccountAddressController extends AbstractController
 
     //Ajouter une addresse
     #[Route('/compte/ajouter-une-adresse', name: 'app_account_address_add')]
-    public function add()
+    public function add(Cart $cart, Request $request)
     {
 
         $address = new Adress();
         $form = $this->createForm(AddressType::class, $address);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $address->setUser($this->getUser());
+            $this->entityManager->persist($address);
+            $this->entityManager->flush();
+            if ($cart->get()) {
+                return $this->redirectToRoute('order');
+            } else {
+                return $this->redirectToRoute('app_account_address');
+            }
+        }
 
         return $this->render('account/address_form.html.twig', [
             'form' => $form->createView()
--- src/Form/AddressType.php
+++ src/Form/AddressType.php
@@ -7,26 +7,81 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 
 class AddressType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('company')
-            ->add('adress')
-            ->add('postal')
-            ->add('city')
-            ->add('county')
-            ->add('phone')
-            ->add('submit', SubmitType::class,[
-                'label' => 'Sauvegarder'
-            ])
-        ;
-    }
+        ->add('name', TextType::class, [
+            'label' => 'Quel nom souhaitez-vous donner à votre adresse ?',
+            'attr' => [
+                'placeholder' => 'Nommez votre adresse'
+            ]
+        ])
+        ->add('firstname', TextType::class, [
+            'label' => 'Votre prénom',
+            'attr' => [
+                'placeholder' => 'Entrez votre prénom'
+            ]
+        ])
+        ->add('lastname', TextType::class, [
+            'label' => 'Votre nom',
+            'attr' => [
+                'placeholder' => 'Entre votre nom'
+            ]
+        ])
+        ->add('company', TextType::class, [
+            'label' => 'Votre société',
+            'required' => false,
+            'attr' => [
+                'placeholder' => '(facultatif) Entrez le nom de votre société'
+            ]
+        ])
+        ->add('adress', TextType::class, [
+            'label' => 'Votre adresse',
+            'attr' => [
+                'placeholder' => '8 rue des lylas ...'
+            ]
+        ])
+        ->add('postal', TextType::class, [
+            'label' => 'Votre code postal',
+            'attr' => [
+                'placeholder' => 'Entrez votre code postal'
+            ]
+        ])
+        ->add('city', TextType::class, [
+            'label' => 'Ville',
+            'attr' => [
+                'placeholder' => 'Entrez votre ville'
+            ]
+        ])
+        ->add('county', CountryType::class, [
+            'label' => 'Pays',
+            'attr' => [
+                'placeholder' => 'Votre pays'
+            ]
+        ])
+        ->add('phone', TelType::class, [
+            'label' => 'Votre téléphone',
+            'attr' => [
+                'placeholder' => 'Entrez votre téléphone'
+            ]
+        ])
+        ->add('submit', SubmitType::class, [
+            'label' => 'Sauvegarder',
+            'attr' => [
+                'class' => 'btn-block btn-info'
+            ]
+        ])
+    ;
+}
+
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
